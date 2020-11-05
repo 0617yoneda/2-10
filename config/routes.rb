014c37083706759ad4484b2e2115a3480d24bb79
EC2 Default User <ec2-user@ip-172-31-73-10.ec2.internal>
2020-11-05 10:17:20 +0000

    [Finish]2-6

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get 'todolists/new' #新規投稿画面
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "top" => "homes#top" #スタート画面
-  post "todolists" => "todolists#create" #スタート画面へ移動
+  post "todolists" => "todolists#create" #詳細画面へ移動
   get "todolists" => "todolists#index" #一覧画面
   get "todolists/:id" => "todolists#show", as:"todolist" #詳細画面
+  get "todolists/:id/edit" => "todolists#edit", as:"edit_todolist" #編集画面
+  patch "todolists/:id" => "todolists#update", as:"update_todolist" #詳細画面へ移動
 end
